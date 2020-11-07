7f596377e40af540a8c7af213577f2c1662ddd5f
romain <rvermo34@gmail.com>
2020-11-07 11:28:32 +0100

    init

---
+++ back/src/main/java/org/rvermorel/nbjardins/NbjardinsApplication.java
@@ -0,0 +1,13 @@
+package org.rvermorel.nbjardins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbjardinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbjardinsApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/org/rvermorel/nbjardins/NbjardinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rvermorel.nbjardins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbjardinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
