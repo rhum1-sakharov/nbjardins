41806522d758e2b46528f8ddebc38f71ca53825b
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2020-12-03 21:20:00 +0100

    import

--- front/src/app/demande-de-devis/demande-de-devis.component.ts
+++ front/src/app/demande-de-devis/demande-de-devis.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MVille} from "../core/models/m-ville";
 import {VillesService} from "../core/services/villes.service";
 import {faPenFancy} from "@fortawesome/free-solid-svg-icons/faPenFancy";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-demande-de-devis',
@@ -11,15 +12,32 @@ import {faPenFancy} from "@fortawesome/free-solid-svg-icons/faPenFancy";
 export class DemandeDeDevisComponent implements OnInit {
 
   villes: MVille[];
-  ville: MVille ;
+  ville: MVille;
   faPenFancy = faPenFancy;
-
 
   devisMessage: string;
 
-  constructor(private villesSvc : VillesService) { }
+  form: FormGroup;
+
+  constructor(private villesSvc: VillesService) {
+  }
 
   ngOnInit(): void {
+
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      nomCtl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      prenomCtl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      societeCtl: new FormControl(''),
+      adresseCtl: new FormControl(''),
+      villeCtl: new FormControl(''),
+      telephoneCtl: new FormControl(''),
+      emailCtl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      messageCtl: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(800)]),
+    });
   }
 
   search($event) {
@@ -27,6 +45,14 @@ export class DemandeDeDevisComponent implements OnInit {
     this.villesSvc.search($event.query).subscribe(response => {
       this.villes = response.map(item => new MVille(item.nom, item.codesPostaux[0]));
     });
+  }
+
+  onSubmit(){
+    if(this.form.valid){
+      console.log('submitting');
+    }else{
+      console.log('form not valid');
+    }
   }
 
 }
