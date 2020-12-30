ca077ad3d6e4d8503dc4d95082f1ff365ca4985d
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2020-12-30 08:52:39 +0100

    import

--- back/adapters/secondaries/flying-saucer-pdf/src/main/java/org/rlsv/adapters/secondaries/flyingsaucerpdf/PdfGeneratorAR.java
+++ back/adapters/secondaries/flying-saucer-pdf/src/main/java/org/rlsv/adapters/secondaries/flyingsaucerpdf/PdfGeneratorAR.java
@@ -1,14 +1,63 @@
 package org.rlsv.adapters.secondaries.flyingsaucerpdf;
 
+import com.lowagie.text.DocumentException;
 import domain.models.DevisDN;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 import ports.pdfs.ProviderPdfPT;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class PdfGeneratorAR implements ProviderPdfPT {
 
+    public static final String REPORT_DEVIS = "devis";
+
+    private TemplateEngine thymeleafTemplateEngine;
+
+    public PdfGeneratorAR() {
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setPrefix("templates/");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        thymeleafTemplateEngine = new TemplateEngine();
+        thymeleafTemplateEngine.setTemplateResolver(templateResolver);
+    }
+
     @Override
     public ByteArrayOutputStream genererDevisPDF(DevisDN devis) {
+
+        Context context = new Context();
+        context.setVariable("devis", devis);
+
+        return processTemplateToPdf(REPORT_DEVIS, context);
+    }
+
+    private ByteArrayOutputStream processTemplateToPdf(String reportName, Context context) {
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+
+            String html = thymeleafTemplateEngine.process(reportName, context);
+
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(html);
+            renderer.layout();
+
+            renderer.createPDF(baos);
+
+            return baos;
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
+
 }
---
+++ back/adapters/secondaries/flying-saucer-pdf/src/test/java/org/rlsv/adapters/secondaries/flyingsaucerpdf/PdfGeneratorARTest.java
@@ -0,0 +1,46 @@
+package org.rlsv.adapters.secondaries.flyingsaucerpdf;
+
+import domain.models.DevisDN;
+import domain.models.PersonneDN;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+public class PdfGeneratorARTest {
+
+    PdfGeneratorAR pdfGeneratorAR;
+
+    @Before
+    public void setUp() throws Exception {
+        this.pdfGeneratorAR = new PdfGeneratorAR();
+    }
+
+    @Test
+    public void genererDevisPDF() {
+        ByteArrayOutputStream baos = pdfGeneratorAR.genererDevisPDF(initDevis());
+
+        try (OutputStream os = new FileOutputStream("test-devis.pdf")) {
+            baos.writeTo(os);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private DevisDN initDevis() {
+
+        PersonneDN worker = new PersonneDN();
+        worker.setNom("Boetsch");
+
+
+        DevisDN devis = new DevisDN();
+
+        devis.setWorker(worker);
+
+        return devis;
+    }
+}
