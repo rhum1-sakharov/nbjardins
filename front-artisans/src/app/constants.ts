ec23012f3b270e4b678b1c75b655e445e3f18c8d
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2021-01-06 19:29:29 +0100

    import

---
+++ front-artisans/src/app/constants.ts
@@ -0,0 +1,6 @@
+export const OAUTH_GOOGLE_ID = '342992225722-f12jtc44h4gvsdn7kepj4nuudn91lfhj.apps.googleusercontent.com';
+export const OAUTH_GOOGLE_SECRET = '_1R5cJ2dpBF8cxt4wIC0VMUZ';
+export const OAUTH_GOOGLE_REDIRECT_URI = `${encodeURI('http://localhost:4200/api/connect')}`;
+export const OAUTH_GOOGLE_CONNECTION_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=email&access_type=online&state=state_parameter_passthrough_value&redirect_uri=${OAUTH_GOOGLE_REDIRECT_URI}&response_type=code&client_id=${OAUTH_GOOGLE_ID}`
+
+
