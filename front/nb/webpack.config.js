7a5d4b8214f09039ebc1064b55768b50a37ce60e
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2021-02-04 23:56:46 +0100

    import

---
+++ front/nb/webpack.config.js
@@ -0,0 +1,15 @@
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// module.exports ={
+//
+//
+//   module: {
+//
+//     rules: [
+//       {
+//         test: /\.scss$/i,
+//         exclude:/node_modules/,
+//         use: ['style-loader', 'fast-css-loader','fast-sass-loader'],
+//       },
+//     ]
+//   }
+// }
