1c1f72ac076db2ec6a8cb9a4f29c07dfa61b6044
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2021-03-03 22:22:10 +0100

    import

--- front/artisans/src/app/pages/parametres/preview-devis/preview-devis.component.spec.ts
+++ front/artisans/src/app/pages/parametres/preview-devis/preview-devis.component.spec.ts
@@ -1,22 +1,52 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PreviewDevisComponent} from './preview-devis.component';
+import {MArtisan, MArtisanBanque, MPersonne} from 'rhum1-sakharov-core-lib';
 
 describe('PreviewDevisComponent', () => {
   let component: PreviewDevisComponent;
   let fixture: ComponentFixture<PreviewDevisComponent>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PreviewDevisComponent ]
+      declarations: [PreviewDevisComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PreviewDevisComponent);
     component = fixture.componentInstance;
+
+    component.artisan = new MArtisan();
+    component.artisan.personne = new MPersonne();
+
+    component.artisanBanque = new MArtisanBanque();
+
     fixture.detectChanges();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have provision paragraph if {{artisan.provision}} greater than 0', () => {
+
+    component.artisan.provision=1;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('[data-test="provision"]')).toBeTruthy();
+  });
+
+  it('should not have provision paragraph if {{artisan.provision}} <= 0', () => {
+
+    component.artisan.provision=0;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('[data-test="provision"]')).toBeFalsy();
+  });
+
+
 
 });
