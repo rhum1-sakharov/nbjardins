f9e4fdbe983cb4305a6046a307b882fb9a0a97dd
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2021-03-25 21:58:18 +0100

    import

--- front/dujardinaupotager/src/app/components/content/me-contacter/me-contacter.component.ts
+++ front/dujardinaupotager/src/app/components/content/me-contacter/me-contacter.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
+import Map from 'ol/Map';
+import View from 'ol/View';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
 @Component({
   selector: 'app-me-contacter',
   templateUrl: './me-contacter.component.html',
@@ -7,9 +13,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MeContacterComponent implements OnInit {
 
+   map: Map;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([5.0698,43.9970 ]),
+        zoom: 12
+      })
+    });
+
   }
 
 }
