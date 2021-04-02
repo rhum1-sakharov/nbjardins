65856b42c0efd79c1df69123fee1cc5d1fc693a2
rhum1-sakharov <rhum1-sakharov@users.noreply.github.com>
2021-04-02 21:00:52 +0200

    import

--- back/adapters/secondaries/dataproviderjpa/src/main/java/org/rlsv/adapters/secondaries/dataproviderjpa/entities/personnes/clients/Client.java
+++ back/adapters/secondaries/dataproviderjpa/src/main/java/org/rlsv/adapters/secondaries/dataproviderjpa/entities/personnes/clients/Client.java
@@ -6,22 +6,31 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.rlsv.adapters.secondaries.dataproviderjpa.entities.Entity;
 import org.rlsv.adapters.secondaries.dataproviderjpa.entities.personnes.Personne;
+import org.rlsv.adapters.secondaries.dataproviderjpa.entities.personnes.artisans.Artisan;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @javax.persistence.Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="clients")
+@Table(name = "clients")
 public class Client extends Entity {
 
     @OneToOne
     @JoinColumn(name = "ID_PERSONNE")
     private Personne personne;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_ARTISAN")
+    private Artisan artisan;
+
+    @Column(name = "SIRET")
+    private String siret;
+
+    @Column(name = "SIGNATURE")
+    private String signature;
 
 
 }
